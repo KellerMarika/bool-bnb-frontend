ea4dc2fb7a4400dd54003ca929e9324d91e446f1
KellerMarika <marikakeller92@gmail.com>
2023-03-03 15:33:44 +0100

    create fixed

--- src/pages/apartments/Create.vue
+++ src/pages/apartments/Create.vue
@@ -23,18 +23,18 @@ export default {
       apartments: [],
 
       form: {
-        user_id:1,
+        user_id: 1,
         title: "Buco Hobbit",
         address: "Hobbiville, Contea",
         latitude: "14.56121",
         longitude: "22.33456",
         cover_img: "",
         description: "Non era un buco brutto, sporco, umido, pieno di vermi e di trasudo fetido, e neanche un buco arido, spoglio, sabbioso, con dentro niente per sedersi o da mangiare: era un buco hobbit, cioè comodissimo.",
-        rooms_qty:5,
+        rooms_qty: 5,
         beds_qty: 2,
-        bathrooms_qty:1,
-        mq: "85",
-        daily_price: "200",
+        bathrooms_qty: 1,
+        mq: 85,
+        daily_price: 200.44,
         visible: 1,
       },
     }
@@ -46,19 +46,19 @@ export default {
       // chiamata axios ad una rotta del server alla quale inviamo i dati del form
       // siccome dobbiamo inviare un file, occorre convertire l'oggetto form in un oggetto FormData
       const formData = new FormData();
-    //  formData.append("user_id", this.form.user_id);
+      //  formData.append("user_id", this.form.user_id);
       formData.append("title", this.form.title);
       formData.append("address", this.form.address);
-     formData.append("latitude", this.form.latitude);
+      formData.append("latitude", this.form.latitude);
       formData.append("longitude", this.form.longitude);
       formData.append("cover_img", this.form.cover_img);
       formData.append("description", this.form.description);
-  /*      formData.append("rooms_qty", this.form.rooms_qty);
+      formData.append("rooms_qty", this.form.rooms_qty);
       formData.append("beds_qty", this.form.beds_qty);
       formData.append("bathrooms_qty", this.form.bathrooms_qty);
       formData.append("mq", this.form.mq);
       formData.append("daily_price", this.form.daily_price);
-      formData.append("visible", this.form.visible); */
+      formData.append("visible", this.form.visible);
 
       api_POST(this.$route.meta.apiRoutePath, formData)
     },
