8d985ebaa83011eee920d8551fad37371b627243
ippoRulez <ippolitodonato@gmail.com>
2023-03-04 18:58:47 +0100

    fixed api calls and cover_img print

--- src/components/SingleCardApartment.vue
+++ src/components/SingleCardApartment.vue
@@ -1,48 +1,50 @@
 <template>
-<h5>Title Apartment</h5>
-<div class="mb-2">
-{{apartment.title}}
-</div>
-<h5>Apartment Description</h5>
-<div class="mb-2">
-{{apartment.description}}
-</div>
-
+	<h5>Title Apartment</h5>
+	<div class="mb-2">
+		{{ apartment.title }}
+	</div>
+	<h5>Apartment Description</h5>
+	<div class="mb-2">
+		{{ apartment.description }}
+	</div>
+	<img
+		:src="this.store.backedRootUrl + '/storage/' + apartment.cover_img"
+		alt="" />
 </template>
 <script>
+import {store} from '../store';
 
 export default {
-   props: {
-      /** 
-       *@param {int} user_id => required|exists:users,id,
-       *@param {string} title => required|string,
-       *@param {string} address => required|string,
-       *@param {string} latitude => required,
-       *@param {string} longitude => required,
-       *@param {string} cover_img => file|image,
-       *@param {string} description => string|max:1000,
-       *@param {int} rooms_qty => required|integer,
-       *@param {int} beds_qty => required|integer,
-       *@param {int} bathrooms_qty => required|integer,
-       *@param {int} mq => integer,
-       *@param {float} daily_price => required|decimal:2,
-       *@param {bool} visible => nullable|boolean,
-       *@param {array} services => 'nullable|array',
-       */
-      apartment: {
-         required: true,
-         type: Object
-      }
-   },
-   data() {
-      return {
-      }
-   },
-   methods: {
-   },
-   mounted() {
-   }
-}
+	props: {
+		/**
+		 *@param {int} user_id => required|exists:users,id,
+		 *@param {string} title => required|string,
+		 *@param {string} address => required|string,
+		 *@param {string} latitude => required,
+		 *@param {string} longitude => required,
+		 *@param {string} cover_img => file|image,
+		 *@param {string} description => string|max:1000,
+		 *@param {int} rooms_qty => required|integer,
+		 *@param {int} beds_qty => required|integer,
+		 *@param {int} bathrooms_qty => required|integer,
+		 *@param {int} mq => integer,
+		 *@param {float} daily_price => required|decimal:2,
+		 *@param {bool} visible => nullable|boolean,
+		 *@param {array} services => 'nullable|array',
+		 */
+		apartment: {
+			required: true,
+			type: Object,
+		},
+	},
+	data() {
+		return {
+			store,
+		};
+	},
+	methods: {},
+	mounted() {},
+};
 </script>
 
 <style lang="scss" scoped></style>
--- src/pages/apartments/Create.vue
+++ src/pages/apartments/Create.vue
@@ -1,281 +1,370 @@
 <template>
-  <section class="contacts d-flex flex-column ">
-    <div class="container flex-fill">
-      <h1 class="mt-3">Create Apartment</h1>
+	<section class="contacts d-flex flex-column">
+		<div class="container flex-fill">
+			<h1 class="mt-3">Create Apartment</h1>
 
-      <div class="alert alert-success" v-if="this.store.submitResult === 'success'">
-        appartamento creato correttamente!
+			<div
+				class="alert alert-success"
+				v-if="this.store.submitResult === 'success'">
+				appartamento creato correttamente!
 
-        <!-- HOME BUTTON -->
-        <router-link @click="store.submitResult = ''"
-            :to="{ name: 'home' }">
-          <button> TORNA ALLA HOME </button>
-        </router-link>
-      </div>
+				<!-- HOME BUTTON -->
+				<router-link @click="store.submitResult = ''" :to="{name: 'home'}">
+					<button>TORNA ALLA HOME</button>
+				</router-link>
+			</div>
 
-      <div class="alert alert-danger" v-else-if="this.store.submitResult">
-        Sembra ci sia stato un errore. Ti invitiamo a riprovare pià tardi.<br />
-        {{ this.store.submitResult }}
-      </div>
+			<div class="alert alert-danger" v-else-if="this.store.submitResult">
+				Sembra ci sia stato un errore. Ti invitiamo a riprovare pià
+				tardi.<br />
+				{{ this.store.submitResult }}
+			</div>
 
-      <form @submit.prevent="onCreateFormSubmit" v-if="this.store.submitResult !== 'success'">
-        <div class="mb-3">
-          <label for="titleInput" class="form-label">Title</label>
-          <input type="text" class="form-control" id="titleInput" v-model="form.title" />
-        </div>
+			<form
+				@submit.prevent="fetchTomTom"
+				v-if="this.store.submitResult !== 'success'">
+				<div class="mb-3">
+					<label for="titleInput" class="form-label">Title</label>
+					<input
+						type="text"
+						class="form-control"
+						id="titleInput"
+						v-model="form.title" />
+				</div>
 
-        <div class="mb-2">
-          <label for="streetNumberInput" class="form-label">Street Number</label>
-          <input type="text" placeholder="Ex. 46" class="form-control" id="streetNumberInput"
-              v-model="formAddress.streetNumber" />
-        </div>
+				<div class="mb-2">
+					<label for="streetNumberInput" class="form-label"
+						>Street Number</label
+					>
+					<input
+						type="text"
+						placeholder="Ex. 46"
+						class="form-control"
+						id="streetNumberInput"
+						v-model="formAddress.streetNumber" />
+				</div>
 
-        <div class="mb-2">
-          <label for="streetNameInput" class="form-label">Street Name</label>
-          <input type="text" placeholder="Ex. via generale cascino" class="form-control" id="streetNameInput"
-              v-model="formAddress.streetName" />
-        </div>
+				<div class="mb-2">
+					<label for="streetNameInput" class="form-label"
+						>Street Name</label
+					>
+					<input
+						type="text"
+						placeholder="Ex. via generale cascino"
+						class="form-control"
+						id="streetNameInput"
+						v-model="formAddress.streetName" />
+				</div>
 
-        <div class="mb-2">
-          <label for="postalCodeInput" class="form-label">Postal Code </label>
-          <input type="number" placeholder="Ex. 93100" class="form-control" id="postalCodeInput"
-              v-model="formAddress.postalCode" />
-        </div>
+				<div class="mb-2">
+					<label for="postalCodeInput" class="form-label"
+						>Postal Code
+					</label>
+					<input
+						type="number"
+						placeholder="Ex. 93100"
+						class="form-control"
+						id="postalCodeInput"
+						v-model="formAddress.postalCode" />
+				</div>
 
-        <div class="mb-2">
-          <label for="countryCodeInput" class="form-label">Country Code</label>
-          <input type="text" placeholder="Ex. IT" class="form-control" id="countryCodeInput"
-              v-model="formAddress.countryCode" />
-        </div>
+				<div class="mb-2">
+					<label for="countryCodeInput" class="form-label"
+						>Country Code</label
+					>
+					<input
+						type="text"
+						placeholder="Ex. IT"
+						class="form-control"
+						id="countryCodeInput"
+						v-model="formAddress.countryCode" />
+				</div>
 
-        <div class="mb-2">
-          <label for="municipalityInput" class="form-label">Municipality</label>
-          <input type="text" placeholder="Ex. roma" class="form-control" id="municipalityInput"
-              v-model="formAddress.municipality" />
-        </div>
+				<div class="mb-2">
+					<label for="municipalityInput" class="form-label"
+						>Municipality</label
+					>
+					<input
+						type="text"
+						placeholder="Ex. roma"
+						class="form-control"
+						id="municipalityInput"
+						v-model="formAddress.municipality" />
+				</div>
 
+				<div class="mb-2">
+					<label for="cover_imgInput" class="form-label"
+						>Apartment Images</label
+					>
+					<input
+						type="file"
+						class="form-control"
+						id="cover_imgInput"
+						@change="onAttachmentChange" />
+				</div>
 
-        <div class="mb-2">
-          <label for="cover_imgInput" class="form-label">Apartment Images</label>
-          <input type="file" class="form-control" id="cover_imgInput" @change="onAttachmentChange" />
-        </div>
+				<div class="mb-2">
+					<label for="descriptionInput" class="form-label"
+						>Description</label
+					>
+					<textarea
+						placeholder="Ex. describe your apartment"
+						class="form-control"
+						id="descriptionInput"
+						v-model="form.description" />
+				</div>
 
-        <div class="mb-2">
-          <label for="descriptionInput" class="form-label">Description</label>
-          <textarea placeholder="Ex. describe your apartment" class="form-control" id="descriptionInput"
-              v-model="form.description" />
-        </div>
+				<div class="mb-2">
+					<label for="rooms_qtyInput" class="form-label"
+						>Rooms Quantity</label
+					>
+					<input
+						type="number"
+						placeholder="number of rooms Ex. 5"
+						class="form-control"
+						id="rooms_qtyInput"
+						v-model="form.rooms_qty" />
+				</div>
 
-        <div class="mb-2">
-          <label for="rooms_qtyInput" class="form-label">Rooms Quantity</label>
-          <input type="number" placeholder="number of rooms Ex. 5" class="form-control" id="rooms_qtyInput"
-              v-model="form.rooms_qty" />
-        </div>
+				<div class="mb-2">
+					<label for="beds_qtyInput" class="form-label"
+						>Beds Quantity</label
+					>
+					<input
+						type="number"
+						placeholder="number of beds Ex. 10"
+						class="form-control"
+						id="beds_qtyInput"
+						v-model="form.beds_qty" />
+				</div>
 
-        <div class="mb-2">
-          <label for="beds_qtyInput" class="form-label">Beds Quantity</label>
-          <input type="number" placeholder="number of beds Ex. 10" class="form-control" id="beds_qtyInput"
-              v-model="form.beds_qty" />
-        </div>
+				<div class="mb-2">
+					<label for="bathrooms_qtyInput" class="form-label"
+						>Bathrooms Quantity</label
+					>
+					<input
+						type="number"
+						placeholder="number of bethrooms Ex. 3"
+						class="form-control"
+						id="bathrooms_qtyInput"
+						v-model="form.bathrooms_qty" />
+				</div>
 
-        <div class="mb-2">
-          <label for="bathrooms_qtyInput" class="form-label">Bathrooms Quantity</label>
-          <input type="number" placeholder="number of bethrooms Ex. 3" class="form-control" id="bathrooms_qtyInput"
-              v-model="form.bathrooms_qty" />
-        </div>
+				<div class="mb-2">
+					<label for="mqInput" class="form-label">Square Meters</label>
+					<input
+						type="number"
+						placeholder="square meters of the apartment Ex. 75"
+						class="form-control"
+						id="mqInput"
+						v-model="form.mq" />
+				</div>
 
-        <div class="mb-2">
-          <label for="mqInput" class="form-label">Square Meters</label>
-          <input type="number" placeholder="square meters of the apartment Ex. 75" class="form-control" id="mqInput"
-              v-model="form.mq" />
-        </div>
+				<div class="mb-2">
+					<label for="daily_priceInput" class="form-label"
+						>Daily Price</label
+					>
+					<input
+						type="number"
+						step=".01"
+						placeholder="daily price of the apartment Ex. 180"
+						class="form-control"
+						id="daily_priceInput"
+						v-model="form.daily_price" />
+				</div>
 
-        <div class="mb-2">
-          <label for="daily_priceInput" class="form-label">Daily Price</label>
-          <input type="number" step=".01" placeholder="daily price of the apartment Ex. 180" class="form-control"
-              id="daily_priceInput" v-model="form.daily_price" />
-        </div>
+				<!--  {{ --visible --}} -->
+				<div class="input-container pb-2 col-12 col-sm-4 col-md-2 ps-3">
+					<div class="form-check form-switch p-0">
+						<label class="form-check-label" for="visible">visible</label>
+						<div class="form-check form-switch pt-2">
+							<input type="hidden" value="1" />
+							<input
+								class="form-check-input"
+								true-value="1"
+								false-value="0"
+								v-model="form.visible"
+								type="checkbox"
+								role="switch"
+								id="visible" />
+						</div>
+					</div>
+				</div>
 
-        <!--  {{ --visible --}} -->
-        <div class="input-container pb-2 col-12  col-sm-4 col-md-2 ps-3">
-          <div class="form-check form-switch p-0">
+				<!--    {{ --services --}} -->
 
-            <label class="form-check-label" for="visible">visible</label>
-            <div class="form-check form-switch pt-2">
+				<div class="input-container pb-2 col-12">
+					<label class="form-label text-uppercase">Services:</label> <br />
+					<div
+						class="form-check form-check-inline"
+						v-for="(service, i) in services">
+						<input
+							type="checkbox"
+							class="form-check-input"
+							:value="service.id"
+							id="service_{{i}}"
+							v-model="form.services" />
 
-              <input type="hidden" value="1">
-              <input
-                  class="form-check-input"
-                  true-value="1" false-value="0" v-model="form.visible" type="checkbox" role="switch" id="visible">
-            </div>
-          </div>
-        </div>
-
-        <!--    {{ --services --}} -->
-
-        <div class="input-container pb-2 col-12 ">
-          <label class="form-label text-uppercase">Services:</label> <br>
-          <div class="form-check form-check-inline" v-for=" service, i in services">
-            <input type="checkbox"
-                class="form-check-input"
-                :value="service.id"
-
-                id="service_{{i}}"
-                v-model="form.services">
-
-            <label class="form-check form-check-inline"
-                for="service_{{i}}">{{ service.name }}
-              <i><img :src="'../../../public/services-icons/' + service.icon" alt=""></i></label>
-          </div>
-
-        </div>
-        <div class="d-flex justify-content-center gap-3">
-          <button class="btn btn-secondary" typeof="reset" :disabled="loading">
-            Annulla
-          </button>
-          <button class="btn btn-success" :disabled="loading">
-            <span v-if="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-            Invia
-          </button>
-        </div>
-      </form>
-    </div>
-  </section>
+						<label
+							class="form-check form-check-inline"
+							for="service_{{i}}"
+							>{{ service.name }}
+							<i
+								><img
+									:src="
+										'../../../public/services-icons/' + service.icon
+									"
+									alt="" /></i
+						></label>
+					</div>
+				</div>
+				<div class="d-flex justify-content-center gap-3">
+					<button
+						class="btn btn-secondary"
+						typeof="reset"
+						:disabled="loading">
+						Annulla
+					</button>
+					<button class="btn btn-success" :disabled="loading">
+						<span
+							v-if="loading"
+							class="spinner-border spinner-border-sm"
+							role="status"
+							aria-hidden="true"></span>
+						Invia
+					</button>
+				</div>
+			</form>
+		</div>
+	</section>
 </template>
 
 <script>
 import axios from 'axios';
-import { titles } from '../../store';
-import { api_POST, store } from '../../store';
+import {titles} from '../../store';
+import {api_POST, store} from '../../store';
 
 export default {
-  name: "Apartments Create",
-  components: {},
-  data() {
-    return {
+	name: 'Apartments Create',
+	components: {},
+	data() {
+		return {
+			store,
+			apartments: [],
+			services: [],
 
-      store,
-      apartments: [],
-      services: [],
+			formAddress: {
+				streetNumber: '',
+				streetName: '',
+				countryCode: '',
+				postalCode: '',
+				municipality: '',
+			},
 
-      formAddress: {
-        streetNumber: "",
-        streetName: "",
-        countryCode: "",
-        postalCode: "",
-        municipality: "",
-      },
+			form: {
+				user_id: 1,
+				title: '',
+				address: '',
+				latitude: '',
+				longitude: '',
+				cover_img: '',
+				description: '',
+				rooms_qty: '',
+				beds_qty: '',
+				bathrooms_qty: '',
+				mq: '',
+				daily_price: '',
+				visible: 0,
+				services: [],
+			},
+		};
+	},
+	methods: {
+		/* RECUOERA LISTA SERVIZI DISPONIBILI PER APPARTAMENTO */
+		fetchServices() {
+			let apiUrl = `${this.store.backedRootUrl}/api/services`;
 
-      form: {
-        user_id: 1,
-        title: "",
-        address: "",
-        latitude: "",
-        longitude: "",
-        cover_img: "",
-        description: "",
-        rooms_qty: "",
-        beds_qty: "",
-        bathrooms_qty: "",
-        mq: "",
-        daily_price: "",
-        visible: 0,
-        services: [],
-      },
-    }
-  },
-  methods: {
-    /* RECUOERA LISTA SERVIZI DISPONIBILI PER APPARTAMENTO */
-    fetchServices() {
+			axios
+				.get(`${apiUrl}`)
+				.then((resp) => {
+					/* console.log(resp) */
+					this.store.submitResult = 'success';
+					this.store.loading = false;
+					this.services = resp.data;
+					// console.log("services", this.services)
+				})
+				.catch((e) => {
+					if (e.response && e.response.data) {
+						this.store.submitResult = e.response.data.message;
+					} else {
+						this.store.submitResult = e.message;
+					}
+					console.log(e);
+				});
+		},
+		/* FORMATTA E INVIA AL BECKEND I DATI  DEL FORM PER LA CREAZIONE DEL NUOVO "APPARTAMENTO" E RELATIVE RELAZIONI */
 
-      let apiUrl = `${this.store.backedRootUrl}/api/services`
+		onCreateFormSubmit() {
+			this.store.loading = true;
+			//Assegno form. addres, longitude e latitude attraverso la chiamata axios
 
-      axios.get(`${apiUrl}`)
-        .then((resp) => {
-          /* console.log(resp) */
-          this.store.submitResult = "success";
-          this.store.loading = false;
-          this.services = resp.data;
-          // console.log("services", this.services)
-        })
-        .catch((e) => {
+			const formData = new FormData();
+			formData.append('user_id', this.form.user_id);
+			formData.append('title', this.form.title);
+			formData.append('address', this.form.address);
+			formData.append('latitude', this.form.latitude);
+			formData.append('longitude', this.form.longitude);
+			formData.append('cover_img', this.form.cover_img);
+			formData.append('description', this.form.description);
+			formData.append('rooms_qty', this.form.rooms_qty);
+			formData.append('beds_qty', this.form.beds_qty);
+			formData.append('bathrooms_qty', this.form.bathrooms_qty);
+			formData.append('mq', this.form.mq);
+			formData.append('daily_price', this.form.daily_price);
+			formData.append('visible', this.form.visible);
 
-          if (e.response && e.response.data) {
-            this.store.submitResult = e.response.data.message;
-          } else {
-            this.store.submitResult = e.message;
-          }
-          console.log(e);
-        });
-    },
-    /* FORMATTA E INVIA AL BECKEND I DATI  DEL FORM PER LA CREAZIONE DEL NUOVO "APPARTAMENTO" E RELATIVE RELAZIONI */
-    onCreateFormSubmit() {
+			for (let i = 0; i < this.form.services.length; i++) {
+				formData.append('services[]', this.form.services[i]);
+			}
 
-      this.store.loading = true;
-      //Assegno form. addres, longitude e latitude attraverso la chiamata axios
-      this.fecthTomTom();
+			api_POST(this.$route.meta.apiRoutePath, formData);
+		},
+		/* RECUPERO COVER IMG DA INPUT FILE ----> QUESTA ANDRA' RIVISTA PER + IMMAGINI */
+		onAttachmentChange(event) {
+			// reucupero l'array dei file scelti dall'utente
+			const chosenFiles = event.target.files;
+			console.log(chosenFiles);
+			// salvo il file scelto dentro l'oggetto form
+			this.form.cover_img = chosenFiles[0];
+		},
 
-      const formData = new FormData();
-      formData.append("user_id", this.form.user_id);
-      formData.append("title", this.form.title);
-      formData.append("address", this.form.address);
-      formData.append("latitude", this.form.latitude);
-      formData.append("longitude", this.form.longitude);
-      formData.append("cover_img", this.form.cover_img);
-      formData.append("description", this.form.description);
-      formData.append("rooms_qty", this.form.rooms_qty);
-      formData.append("beds_qty", this.form.beds_qty);
-      formData.append("bathrooms_qty", this.form.bathrooms_qty);
-      formData.append("mq", this.form.mq);
-      formData.append("daily_price", this.form.daily_price);
-      formData.append("visible", this.form.visible);
+		fetchTomTom() {
+			axios
+				.get(this.store.geoApiUrl + this.store.geoApiKey, {
+					params: this.formAddress,
+				})
+				.then((resp) => {
+					this.form.address = resp.data.results[0].address.freeformAddress;
+					this.form.latitude = resp.data.results[0].position.lat;
+					this.form.longitude = resp.data.results[0].position.lon;
 
-      for (let i = 0; i < this.form.services.length; i++) {
-        formData.append('services[]', this.form.services[i]);
-      }
-
-    api_POST(this.$route.meta.apiRoutePath, formData)
-    },
-    /* RECUPERO COVER IMG DA INPUT FILE ----> QUESTA ANDRA' RIVISTA PER + IMMAGINI */
-    onAttachmentChange(event) {
-      // reucupero l'array dei file scelti dall'utente
-      const chosenFiles = event.target.files
-      console.log(chosenFiles);
-      // salvo il file scelto dentro l'oggetto form
-      this.form.cover_img = chosenFiles[0];
-    },
-
-    fecthTomTom() {
-
-      axios.get(this.store.geoApiUrl + this.store.geoApiKey, {
-        params: this.formAddress
-      })
-        .then((resp) => {
-          this.form.address = resp.data.results[0].address.freeformAddress;
-          this.form.latitude = resp.data.results[0].position.lat;
-          this.form.longitude = resp.data.results[0].position.lon;
-        })
-
-
-    },
-  },
-  mounted() {
-    titles(this.$route.meta.title);
-    this.fetchServices();
-  },
-  beforeUpdate() {
-    //reset submitREsult
-    this.store.submitResult = ""
-  },
-  created() {
-  }
-}
+					this.onCreateFormSubmit();
+				});
+		},
+	},
+	mounted() {
+		titles(this.$route.meta.title);
+		this.fetchServices();
+	},
+	beforeUpdate() {
+		//reset submitREsult
+		this.store.submitResult = '';
+	},
+	created() {},
+};
 </script>
 
-
 <style lang="scss" scoped>
-@use "../../styles/generic.scss";
-@use "../../styles/partials/variables" as *;
-
-
+@use '../../styles/generic.scss';
+@use '../../styles/partials/variables' as *;
 </style>
