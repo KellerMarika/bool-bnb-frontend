ded1907a749f95d2a6b1b894ff23f232efe7e0aa
LucaConiglio <lucaconiglio.92@gmail.com>
2023-03-06 17:52:40 +0100

    adds images + button

--- src/pages/apartments/Show.vue
+++ src/pages/apartments/Show.vue
@@ -1,33 +1,81 @@
 <template>
   <div class="container">
-    <h1>Apartments Show</h1>
+    <!--Apartments Show -->
 
-    <h3>{{ apartment.title }}</h3>
+    <h1 class="mb-4">{{ apartment.title }}</h1>
+    <div v-if="apartment.images" class="row">
+      <div class="col-6 mb-2 ">
+        <img class="img-fluid mb-2 card-img container-cards-left "
+          :src="apartment.images[0].image"
+          alt="" />
+      </div>
+      <div class="col-6">
+        <div class="row">
+          <div v-for="(img, index) in apartment.images"  key="index" v-bind:class="(index ===0) ? ' ' : 'col-6' ">
+            
+            <img v-if="index > 0" :class="(index === 2) ? 'container-cards-top-right' : '' | (index === 4) ? 'container-cards-bottom-right' : ''  "  
+             class="img-fluid mb-2 card-img "
+              :src="img.image"
+              :alt="'image ' + (index + 1)" />
+            
+          </div>
 
-    <h5><i class="fa-solid fa-map-location-dot"></i></h5>
-    <div class="mb-2">
-      {{ apartment.address }}
+
+        </div>
+      </div>
+
     </div>
-    <h5>Rooms Quantity</h5>
-    <div class="mb-2">
-      {{ apartment.rooms_qty }}
+
+    <div v-else class="col-8 m-auto">
+
+      <img class="img-fluid mb-2 card-img"
+          :src="apartment.cover_img"
+          alt="" />
+
     </div>
-    <h5><i class="fa-solid fa-bed"></i></h5>
-    <div class="mb-2">
-      {{ apartment.beds_qty }}
+    <!-- <div class="col-6 container-cards">
+          <img class="img-fluid container-cards-left mb-2 card-img  "
+            :src="this.apartment.images[0].image"
+            alt="" />
+        </div>
+        <div class="col-6">
+          <div v-for="(img, i) in apartment.images" class="row">
+            <div  class="col-6 mb-2"><img class="img-fluid mb-2 card-img "
+                :src="img.image"
+                alt="" /></div>
+          </div>
+        </div> -->
+
+
+
+
+    <div class="my-2">
+      <i class="h5 me-2 fa-solid fa-map-location-dot"></i> {{ apartment.address }}
     </div>
-    <h5><i class="fa-solid fa-bath"></i></h5>
-    <div class="mb-2">
-      {{ apartment.bathrooms_qty }}
+    <hr>
+    <div class="d-flex mb-3">
+
+      <div class="mb-2 mx-2">
+        Rooms {{ apartment.rooms_qty }} |
+      </div>
+
+      <div class="mb-2 mx-2">
+        <i class="fa-solid fa-bed"></i> {{ apartment.beds_qty }} |
+      </div>
+
+      <div class="mb-2 mx-2">
+        <i class="fa-solid fa-bath"></i> {{ apartment.bathrooms_qty }} |
+      </div>
+
+      <div class="mb-2 mx-2">
+        MQ {{ apartment.mq }} |
+      </div>
+      <div class="mx-2">
+        <i class="fa-solid fa-euro-sign"></i> {{ apartment.daily_price }} night
+      </div>
+
     </div>
-    <h5>Square Meters</h5>
-    <div class="mb-2">
-      {{ apartment.mq }}
-    </div>
-    <h5>Daily Price <i class="fa-regular fa-money-bill-1"></i></h5>
-    <div class="mb-2">
-      {{ apartment.daily_price }}
-    </div>
+
 
 
 
@@ -37,6 +85,8 @@
     <ButtonDelete :is="ButtonDelete" />
   </router-link> -->
     <ButtonDelete @click="onDeleteClick()" />
+
+    <a href="http://localhost:5173/apartments" class="btn btn-info ms-2 text-light">RETURN TO INDEX</a>
 
   </div>
 </template>
@@ -130,4 +180,28 @@ export default {
 <style lang="scss" scoped>
 @use "../../styles/generic.scss";
 @use "../../styles/partials/variables" as *;
+
+
+
+.col-6 {
+
+  .container-cards-left {
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+
+  }
+
+  .container-cards-top-right {
+    border-top-right-radius: 20px;
+
+
+  }
+
+  .container-cards-bottom-right {
+
+    border-bottom-right-radius: 20px;
+
+  }
+
+}
 </style>
