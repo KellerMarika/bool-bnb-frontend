25da0279e2f1a8303462f1ffb684ef583bbff10d
LucaConiglio <lucaconiglio.92@gmail.com>
2023-03-14 18:23:31 +0100

    change show map

--- src/pages/apartments/Show.vue
+++ src/pages/apartments/Show.vue
@@ -1,5 +1,5 @@
 <template>
-		<section
+	<section
 		class="px-xxl-5 mx-3 mx-sm-5 mx-lg-5"
 		:class="apartment.images && apartment.images.length ? '' : 'd-flex'">
 		<div class="container.fluid px-md-3 px-xl-5">
@@ -65,87 +65,95 @@
 					class="img-fluid w-100" />
 			</div>
 
-			<!-- MESSAGGIO TRISTE -->
+			<div class="row row-cols-1 row-cols-lg-2 my-4">
+				<div class="col">
+					<!-- MESSAGGIO TRISTE -->
 
-			<div class="my-2">
-				<i class="h5 me-2 fa-solid fa-map-location-dot"></i>
-				{{ apartment.address }}
-			</div>
-			<hr />
-			<div class="d-flex mb-3">
-				<div class="mb-2 mx-2">Rooms {{ apartment.rooms_qty }} |</div>
+					<div class="my-2">
+						<i class="h5 me-2 fa-solid fa-map-location-dot"></i>
+						{{ apartment.address }}
+					</div>
+					<hr />
+					<div class="d-flex mb-3">
+						<div class="mb-2 mx-2">Rooms {{ apartment.rooms_qty }} |</div>
 
-				<div class="mb-2 mx-2">
-					<i class="fa-solid fa-bed"></i> {{ apartment.beds_qty }} |
+						<div class="mb-2 mx-2">
+							<i class="with-icon fa-solid fa-bed"></i> {{ apartment.beds_qty }} |
+						</div>
+
+						<div class="mb-2 mx-2">
+							<i class="with-icon fa-solid fa-bath"></i> {{ apartment.bathrooms_qty }} |
+						</div>
+
+						<div class="mb-2 mx-2">MQ {{ apartment.mq }} |</div>
+						<div class="mx-2">
+							<i class="with-icon fa-solid fa-euro-sign"></i>
+							{{ apartment.daily_price }} night
+						</div>
+					</div>
+					<hr />
+					<div class="mb-2 mx-2">
+						<p>{{ apartment.description }}</p>
+					</div>
+					<hr />
+
+					<div class="mb-4 mx-2 col-8">
+						<div class="fw-semibold">Cosa troverai:</div>
+						<div class="d-flex gap-3">
+							<span
+								class="d-flex gap-2 my-services"
+								v-for="service in apartment.services">
+								<img class="with-icon" :src="'/public/services-icons/' + service.icon" alt="" />
+								{{ service.name }}
+							</span>
+						</div>
+					</div>
+
+					<div class="my-3 d-flex align-items-baseline justify-content-between">
+						<router-link :to="{ name: 'home' }" class="btn btn-info">
+							<!-- CARD -->
+							<h5 class="my-3 mx-4">
+								<i class="fa-solid fa-home"></i>
+								Back to home
+							</h5>
+						</router-link>
+						<a
+							:href="
+								store.backedRootUrl +
+								'/apartments/messages/' +
+								this.apartment.id
+							"
+							class="btn btn-primary">
+							<h5 class="my-3 mx-4">
+								Chiedi maggiori informazioni
+								<i class="fa-solid fa-envelope"></i>
+							</h5>
+						</a>
+					</div>
 				</div>
-
-				<div class="mb-2 mx-2">
-					<i class="fa-solid fa-bath"></i> {{ apartment.bathrooms_qty }} |
+				<div
+					v-if="this.submitResult === 'success'"
+					class="alert alert-success m-3">
+					L'invio è andato a buon fine! Grazie per avermi contattato.
 				</div>
-
-				<div class="mb-2 mx-2">MQ {{ apartment.mq }} |</div>
-				<div class="mx-2">
-					<i class="fa-solid fa-euro-sign"></i>
-					{{ apartment.daily_price }} night
+				<div v-else-if="this.submitResult" class="alert alert-danger m-3">
+					<p>There was a problem with your request:</p>
+					<p v-if="!errors">
+						{{ this.submitResult }}
+					</p>
+					<ul v-else v-for="(error, i) in this.errors">
+						<li>{{ `${i}: ${error}` }}</li>
+					</ul>
 				</div>
-			</div>
-			<hr />
-			<div class="mb-2 mx-2">
-				<p>{{ apartment.description }}</p>
-			</div>
-			<hr />
-
-			<div class="mb-4 mx-2 col-8">
-				<div class="fw-semibold">Cosa troverai:</div>
-				<div class="d-flex gap-3">
-					<span
-						class="d-flex gap-2 my-services"
-						v-for="service in apartment.services">
-						<img :src="'/public/services-icons/' + service.icon" alt="" />
-						{{ service.name }}
-					</span>
+				<div class="col ">
+					<div>
+				<div id="map" class="map  border rounded-3 mb-5 m-auto ms-3 " style="width: 500px; height: 350px; ">
 				</div>
 			</div>
-
-			<div class="my-3 d-flex align-items-baseline justify-content-between">
-				<router-link :to="{name: 'home'}" class="btn btn-info">
-					<!-- CARD -->
-					<h5 class="my-3 mx-4">
-						<i class="fa-solid fa-home"></i>
-						Back to home
-					</h5>
-				</router-link>
-				<a
-					:href="
-						store.backedRootUrl +
-						'/apartments/messages/' +
-						this.apartment.id
-					"
-					class="btn btn-primary">
-					<h5 class="my-3 mx-4">
-						Chiedi maggiori informazioni
-						<i class="fa-solid fa-envelope"></i>
-					</h5>
-				</a>
 			</div>
+			</div>
+			
 		</div>
-		<div
-			v-if="this.submitResult === 'success'"
-			class="alert alert-success m-3">
-			L'invio è andato a buon fine! Grazie per avermi contattato.
-		</div>
-		<div v-else-if="this.submitResult" class="alert alert-danger m-3">
-			<p>There was a problem with your request:</p>
-			<p v-if="!errors">
-				{{ this.submitResult }}
-			</p>
-			<ul v-else v-for="(error, i) in this.errors">
-				<li>{{ `${i}: ${error}` }}</li>
-			</ul>
-		</div>
-		<div id="map" class="map border" style="width: 600px; height: 600px">
-	</div>
-
 	</section>
 </template>
 
@@ -193,8 +201,8 @@ export default {
 				object: '',
 				message: '',
 
-				map:null,
-				marker:null,
+				map: null,
+				marker: null,
 			},
 		};
 	},
@@ -219,86 +227,86 @@ export default {
 
 			//marker
 			this.marker = new tt.Marker().setLngLat([lon, lat])
-			.setPopup(new tt.Popup({ offset: 35 }).setHTML( popName))
-			.addTo(this.map);
-		
-    
- 
+				.setPopup(new tt.Popup({ offset: 35 }).setHTML(popName))
+				.addTo(this.map);
+
+
+
+		},
+
+
+		/**FUNZIONE API CALL SHOW (show).........................
+			*
+			* @param {string} thisRoutePath  es= 'apartments/create'
+			* @param {object} payload es=  {pagination:3}
+			*/
+		api_SHOW(thisRoutePath, payload) {
+			let apiUrl = `${this.store.backedRootUrl}/api${thisRoutePath}${this.$route.params.id}`;
+			axios
+				.get(`${apiUrl}`, {
+					params: payload,
+				})
+				.then((resp) => {
+					if (resp.data === 'error') {
+						this.$router.push('http://localhost:5173/error');
+					}
+					this.store.submitResult = 'success';
+					this.store.loading = false;
+
+					console.log('APPARTAMENTO', resp.data);
+					this.apartment = resp.data;
+					console.log(resp.data);
+
+					/* MAPPA */
+					this.createMap(this.apartment.longitude, this.apartment.latitude, this.apartment.title + '<br/>' + this.apartment.address)
+
+				})
+				.catch((e) => {
+					if (e.response && e.response.data) {
+						this.store.submitResult = e.response.data.message;
+					} else {
+						this.store.submitResult = e.message;
+					}
+					console.log(e);
+				});
+		},
+		onMessageFormSubmit() {
+			this.loading = true;
+
+			const formData = new FormData();
+			formData.append('apartment_id', this.$route.params.id);
+			formData.append('sender', this.messageFormInput.name);
+			formData.append('email', this.messageFormInput.email);
+			formData.append('subject', this.messageFormInput.object);
+			formData.append('message', this.messageFormInput.message);
+
+			console.log(this.messageFormInput);
+
+			axios
+				.post(store.backedRootUrl + '/api/messages', formData)
+				.then((resp) => {
+					this.submitResult = 'success';
+					this.loading = false;
+				})
+				.catch((error) => {
+					this.loading = false;
+
+					if (error.response && error.response.data) {
+						this.submitResult = error.response.data.message;
+						var errorsArray = error.response.data.errors;
+						this.errors = errorsArray;
+						console.log(this.errors);
+					} else {
+						this.submitResult = error.message;
+					}
+				});
+		},
 	},
-
-
-	/**FUNZIONE API CALL SHOW (show).........................
-		*
-		* @param {string} thisRoutePath  es= 'apartments/create'
-		* @param {object} payload es=  {pagination:3}
-		*/
-	api_SHOW(thisRoutePath, payload) {
-		let apiUrl = `${this.store.backedRootUrl}/api${thisRoutePath}${this.$route.params.id}`;
-		axios
-			.get(`${apiUrl}`, {
-				params: payload,
-			})
-			.then((resp) => {
-				if (resp.data === 'error') {
-					this.$router.push('http://localhost:5173/error');
-				}
-				this.store.submitResult = 'success';
-				this.store.loading = false;
-
-				console.log('APPARTAMENTO', resp.data);
-				this.apartment = resp.data;
-				console.log(resp.data);
-
-				/* MAPPA */
-				this.createMap(this.apartment.longitude, this.apartment.latitude, this.apartment.title+ '<br/>'+ this.apartment.address )
-
-			})
-			.catch((e) => {
-				if (e.response && e.response.data) {
-					this.store.submitResult = e.response.data.message;
-				} else {
-					this.store.submitResult = e.message;
-				}
-				console.log(e);
-			});
+	mounted() {
+		titles(this.$route.meta.title + this.$route.params.id);
+		this.api_SHOW(this.$route.meta.apiRoutePath, this.$route.params);
 	},
-	onMessageFormSubmit() {
-		this.loading = true;
-
-		const formData = new FormData();
-		formData.append('apartment_id', this.$route.params.id);
-		formData.append('sender', this.messageFormInput.name);
-		formData.append('email', this.messageFormInput.email);
-		formData.append('subject', this.messageFormInput.object);
-		formData.append('message', this.messageFormInput.message);
-
-		console.log(this.messageFormInput);
-
-		axios
-			.post(store.backedRootUrl + '/api/messages', formData)
-			.then((resp) => {
-				this.submitResult = 'success';
-				this.loading = false;
-			})
-			.catch((error) => {
-				this.loading = false;
-
-				if (error.response && error.response.data) {
-					this.submitResult = error.response.data.message;
-					var errorsArray = error.response.data.errors;
-					this.errors = errorsArray;
-					console.log(this.errors);
-				} else {
-					this.submitResult = error.message;
-				}
-			});
-	},
-},
-mounted() {
-	titles(this.$route.meta.title + this.$route.params.id);
-	this.api_SHOW(this.$route.meta.apiRoutePath, this.$route.params);
-},
-created() { },
+	created() { },
 };
 </script>
 
@@ -321,6 +329,7 @@ created() { },
 			transform: scaleY(120%) translateY(5%);
 		}
 	}
+
 	.row-dx {
 		transform: scaleY(115%) scaleX(102%) translate(10px);
 		position: relative;
@@ -328,10 +337,12 @@ created() { },
 		left: 0;
 	}
 }
+
 .my-services {
 	img {
 		width: 20px;
 	}
 }
+
 
 </style>
